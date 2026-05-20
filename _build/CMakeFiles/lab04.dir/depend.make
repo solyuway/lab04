6688a6316b6990dd11ba88d192285f7439a8b3fc
{GITHUB_USERNAME} <{GITHUB_EMAIL}>
2026-05-20 19:49:17 -0400

    Fix CI: clean CMake cache before build
    - Remove cached CMake files from repository
    - Update .gitignore to exclude build artifacts
    - Add cleanup step before build in Travis CI
    - Add cleanup step before build in AppVeyor

--- _build/CMakeFiles/lab04.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lab04.
-# This may be replaced when dependencies are built.
--- _build/CMakeFiles/lab04.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lab04.dir/src/main.cpp.o: \
- /home/alex/solyuway/workspace/tasks/lab04/src/main.cpp \
- /usr/include/stdc-predef.h
